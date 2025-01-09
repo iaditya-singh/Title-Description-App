9774d03fea3f62bdf4affb36404bcf99f6b593a1
iadityasingh-3616 <iadityasingh2468@gmail.com>
2025-01-10 04:01:45 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
-import React, { useState } from "react";
-import Navbar from "./components/navbar";
+import React from "react";
+import Navbar from "./components/Navbar";
 import Form from "./components/Form";
 import CardContainer from "./components/CardContainer";
 import Footer from "./components/Footer";
 import { TodoProvider } from "./components/TodoContext";
 
 const App = () => {
-
-  
-
-
   return (
     <TodoProvider>
       <div className="relative min-h-screen">
-      <Navbar />
+        <Navbar />
 
-      <div className="container mx-auto p-10">
-        <Form />
-        <CardContainer  />
+        <div className="container mx-auto p-10">
+          <Form />
+          <CardContainer />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
     </TodoProvider>
   );
 };
